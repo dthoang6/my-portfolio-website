e8683ccea5812cee1a2b949b746a42e62da19e49
Dat Hoang <dthoang6@gmail.com>
2022-02-16 17:13:47 -0800

    Using ReactJS to build my porfolio site

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        <span>&copy;</span>
+        TomHoangDev. All rights reserved.
+      </small>
+    </footer>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const projects = [
+  {
+    title: "AirBnB Experiences clone",
+    subtitle: "Data-Driven React",
+    description:
+      "Using Data-Driven to build an AirBnB Clones. The lifecycle of the application are all driven by the flow of the data through the application. User interactions trigger data updates and other possible side-effects.",
+    image: "./AirBnB.jpg",
+    link: "https://kinhdoanhusa.com",
+  },
+  {
+    title: "Meme Generator",
+    subtitle: "Interactive web apps in React",
+    description:
+      "Build a Dynamic web apps that user can read and write: Ability to change data, Highly interactive.",
+    image: "./MemeGenerator.jpg",
+    link: "https://bankmoneydeal.com",
+  },
+  {
+    title: "Tenzied Game",
+    subtitle: "React",
+
+    description: "Build a Tenzied Game.",
+    image: "./Tenzies.jpg",
+    link: "https://bankmoneydeal.com",
+  },
+  {
+    title: "Quiz App",
+    subtitle: "React",
+    description: "Build a Quiz App",
+    image: "./quiz.jpg",
+    link: "https://bankmoneydeal.com",
+  },
+];
+
+export const works = [
+  {
+    title: "Listing Website",
+    subtitle: "JQuery, PHP, Wordpress",
+    description:
+      "Using JQuery, Wordpress CMS to build a listing website for Vietnamese American.",
+    image: "./kinhdoanh.png",
+    link: "https://kinhdoanhusa.com",
+  },
+  {
+    title: "News Website",
+    subtitle: "Auto blogging, Wordpress, PHP",
+    description: "Build a News website and Forums by auto posting articles.",
+    image: "./tomforex.png",
+    link: "https://tomforextrader.com",
+  },
+  {
+    title: "Courses Website",
+    subtitle: "Wordpress, Online Course, PHP",
+    description: "Build a Educational Website with online Courses.",
+    image: "./lamgiau.png",
+    link: "https://lamgiaukieumy.com",
+  },
+  {
+    title: "Restaurant Website",
+    subtitle: "PHP, Wordpress",
+    description: "Build a restaurant website with Menu, About, Contact Page.",
+    image: "./pho18.png",
+    link: "https://pho18hot.com",
+  },
+];
+export const skills = [
+  "JavaScript",
+  "HTML5",
+  "CSS3",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
