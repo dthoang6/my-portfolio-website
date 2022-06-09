23435826309118b4a7d787815d127679f60c9057
Dat Hoang <dthoang6@gmail.com>
2022-06-09 16:05:38 -0700

    edit work component

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ export default function App() {
     <div className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
-      <Projects />
       <Work />
+      <Projects />
       <Skills />
       <Contact />
       <Footer />
